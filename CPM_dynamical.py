155b2871e0d52b0efb5f541b6b707220ae3dce9d
meteoNL <cirrocumulus8@gmail.com>
2018-10-05 11:18:52 +0000

    water vapor included, only mixing, no cond/evap

--- CPM_dynamical.py
+++ CPM_dynamical.py
@@ -11,14 +11,15 @@ g=9.81
 cp=1005
 tend=3600.
 dt=0.5
-gamma=0.0
-mu=3e-4
+gamma=0.5
+mu=5e-4
 
-t1=np.linspace(0,tend,(tend/dt)) 
+t1=np.linspace(0.0,tend,int(tend/dt)) 
 
-Tp=307 #temp air parcel, K
-zp=100
+Tp=308 #temp air parcel, K
+zp=170
 w=0
+wvp=15.0
 
 data_env=np.array([])
 data_p=np.array([])
@@ -27,12 +28,12 @@ f=open('dataset.txt','r')
 i=0
 for line in f:
     line=line.split(';')
-    print(line[2])
     z=float(line[2])
     T=float(line[3])+273.15
-    data_env=np.append(data_env,np.array([z,T]))
+    wv=float(line[6])
+    data_env=np.append(data_env,np.array([z,T,wv]))
     i+=1
-data_env=np.reshape(data_env,(int(len(data_env)/2.0),2))
+data_env=np.reshape(data_env,(int(len(data_env)/3.0),3))
 f.close()
 def dwdt(Tp,Tenv):
     return 1/(1+gamma)*(g*(Tp-Tenv)/Tenv-mu*abs(w)*w)
@@ -40,11 +41,15 @@ def dwdt(Tp,Tenv):
 def dTpdt(w,Tp,zp):
     return -(g*w/cp+mu*abs(w)*(Tp-Tenv(zp)))
 
+def dwvdt(w,wvp,wvenv):
+    return -mu*(wvp-wvenv)*abs(w)
+    
+
 def Tenv(z):
     j=0
     if data_env[j,0] == z or data_env[j,0] > z: #to prevent it from leaving domain
         T=data_env[0,1]
-    if data_env[-1,0] < z: #to prevent it from leaving the domain
+    elif data_env[-1,0] < z: #to prevent it from leaving the domain
         T=300
     while data_env[j,0] < z:
         Tenv_plus1=data_env[(j+1),1]
@@ -55,22 +60,44 @@ def Tenv(z):
         T=Tenv_0+(z-zenv_0)*dTdz
         j+=1
     return T
-print(zp)
+
+def wvenv(z):
+    j=0
+    if data_env[j,0] == z or data_env[j,0] > z: #to prevent it from leaving domain
+        wv=data_env[0,2]
+    elif data_env[-1,0] < z: #to prevent it from leaving the domain
+        wv=0
+    while data_env[j,0] < z:
+        wvenv_plus1=data_env[(j+1),2]
+        wvenv_0=data_env[j,2]
+        zenv_plus1=data_env[(j+1),0]
+        zenv_0=data_env[j,0]
+        dwvdz=(wvenv_plus1-wvenv_0)/(zenv_plus1-zenv_0)
+        wv=wvenv_0+(z-zenv_0)*dwvdz
+        j+=1
+    return wv
+
 for t in t1:
     Tenv_new=Tenv(zp)
+    wvenv_new=wvenv(zp)
     w_old=w
     zp_old=zp
     Tp_old=Tp
+    wv_old=wvp
     w=w+dwdt(Tp,Tenv_new)*dt
     zp=zp+w_old*dt
     Tp=Tp+dTpdt(w_old,Tp_old,zp_old)*dt
-    data_p=np.append(data_p,np.array([zp,Tp,w]))
+    wvp=wvp+dwvdt(w_old,wv_old,wvenv_new)*dt
+    data_p=np.append(data_p,np.array([zp,Tp,w,wvp,Tenv_new,wvenv_new]))
 
-data_p=np.reshape(data_p,(int(len(data_p)/3.0),3))
-pl.plot(data_env[:,1],data_env[:,0])
+data_p=np.reshape(data_p,(int(len(data_p)/6.0),6))
+pl.plot(data_p[:,4],data_p[:,0])
 pl.plot(data_p[:,1],data_p[:,0])
 pl.ylim(0,1.1*np.max(data_p[:,0]))
 pl.show()
+
+pl.plot(data_p[:,5],data_p[:,0])
+pl.plot(data_p[:,3],data_p[:,0])
 
 data=np.zeros((3,len(t1)+1))
 
